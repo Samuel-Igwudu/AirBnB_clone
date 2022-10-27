2eb5b9f20f9961ccb4dba502270251d0cf0f3ef8
Samuel-Igwudu <igwudu03@gmail.com>
2022-10-27 05:41:09 -0700

    AirBnB project console and models

---
+++ console.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+entry point of the command interpreter
+"""
+import cmd
+from models import storage
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """ command interpreter
+    """
+    prompt = "(hbnb) "
+    classname = ['BaseModel']
+
+    def do_quit(self, line):
+        """ quit command """
+        return True
+
+    def do_EOF(self, line):
+        """ EOF command """
+        return True
+
+    def emptyline(self):
+        """ do nothing """
+        pass
+
+    def do_create(self, arg):
+        """ creates a new instance of BaseModel """
+
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.classname:
+            print("** class doesn't exist **")
+        else:
+            dct = {'BaseModel': BaseModel}
+            my_model = dct[arg]()
+            print(my_model.id)
+            my_model.save()
+
+    def do_show(self, arg):
+        """ show string representation of an instance """
+
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split(' ')
+
+        if args[0] not in HBNBCommand.classname:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            objs = storage.all()
+            for key, value in objs.items():
+                obj_name = value.__class.__name__
+                obj_id = value.id
+                if obj_name == args[0] and obj_id == args[1].strp('"'):
+                    print(value)
+                    return
+                print("** no instance found **")
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+class FileStorage that serializes instances to a JSON file and deserializes
+JSON file to instances
+"""
+import json
+import os
+
+
+class FileStorage:
+    """ serializes and deserializes json file
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ return dict __objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id
+        """
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)
+        """
+        dictionary = {}
+
+        for key, value in FileStorage.__objects.items():
+            dictionary[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(dictionary, f)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects
+        """
+        from models.base_model import BaseModel
+        attr = {'BaseModel': BaseModel}
+
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.new(dct[value['__class__']](**value))
